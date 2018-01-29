e5144a76b0c9bd22de6e14573c2c82685ff62d4b
Noe Lomeli <nolomeli@csumb.edu>
2018-01-29 14:39:38 -0800

    Added pod dependencies

---
+++ Pods/Target Support Files/Pods-Tumblr-PhotoFeed/Pods-Tumblr-PhotoFeed-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Tumblr_PhotoFeed : NSObject
+@end
+@implementation PodsDummy_Pods_Tumblr_PhotoFeed
+@end
---
+++ Pods/Target Support Files/Pods-Tumblr-PhotoFeedUITests/Pods-Tumblr-PhotoFeedUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Tumblr_PhotoFeedUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Tumblr_PhotoFeedUITests
+@end
